797cf98a2db3ea256374d162ee480bd92351074e
Anna <ruffeya@gmail.com>
2025-02-17 21:19:45 -0600

    add mui material and container to the host

--- react-host/src/bootstrap.tsx
+++ react-host/src/bootstrap.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Button from 'Remote/Button';
+import { Container, CssBaseline } from '@mui/material';
+
 import ActionsList from 'Remote/ActionsList';
 
 const App = () => (
-  <div>
-    <h1>Hello from Host</h1>
-    <Button />
-    <ActionsList />
-  </div>
+  <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="lg">
+      <ActionsList />
+    </Container>
+  </React.Fragment>
 );
 
 
 const domNode = document.getElementById('root');
 if (domNode) {
   const root = createRoot(domNode);
-
   root.render(<App />);
 } else {
   console.error('Root element not found');
