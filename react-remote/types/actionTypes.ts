c23a0f0f3bb17d5e8709039e250e5e0635d475b2
Anna <ruffeya@gmail.com>
2025-02-18 21:21:58 -0600

    create an array of formTypes

--- react-remote/types/actionTypes.ts
+++ react-remote/types/actionTypes.ts
@@ -1,13 +1,17 @@
-export type FormType = 
-  | 'CleanTheFridge'
-  | 'BuyGroceries'
-  | 'VacuumTheLivingAreas'
-  | 'DustFurnitureAndShelves'
-  | 'WashTheBedding'
-  | 'ScrubTheBathroom'
-  | 'MopTheFloors'
-  | 'TakeOutTheTrashAndRecycling'
-  | 'WaterThePlants';
+
+export const formTypes = [
+  'CleanTheFridge',
+  'BuyGroceries',
+  'VacuumTheLivingAreas',
+  'DustFurnitureAndShelves',
+  'WashTheBedding',
+  'ScrubTheBathroom',
+  'MopTheFloors',
+  'TakeOutTheTrashAndRecycling',
+  'WaterThePlants'
+] as const;
+
+export type FormType = typeof formTypes[number];
 
 export type ActionType = {
   id: number;
